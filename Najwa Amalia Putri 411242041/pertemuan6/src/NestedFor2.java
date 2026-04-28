cf451d1d082c4605134b9551204b21326a52fde3
Najwa2907 <29putriamalia@gmail.com>
2026-04-28 20:41:49 +0700

    Add files via upload

---
+++ Najwa Amalia Putri 411242041/pertemuan6/src/NestedFor2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Mahasiswa
+ */
+public class NestedFor2 {
+    public static void main(String[] args) {
+        for(int a=1; a<=5; a++){
+            for(int b=1; b<=a; b++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+                                                              
---
+++ Najwa Amalia Putri 411242041/pertemuan6/src/pertemuan6/Pertemuan6.java
@@ -0,0 +1,27 @@
+
+FOR (int/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan6;
+
+/**
+ *
+ * @author Mahasiswa
+ */
+public class Pertemuan6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i =1; i <= 10; i++){
+            
+            System.out.println("perulangan ke" +1);
+            if (i >= 4)break;
+            
+        }
+    }
+    
+}
